5acf0f232c4f2e7fb5cc616ca84548fc62a66473
xqc <xqc>
2020-12-06 20:41:26 +0800

    task:用户模块初始化

---
+++ backend_user/src/test/java/com/meetingfilm/user/utils/UtilsTest.java
@@ -0,0 +1,20 @@
+package com.meetingfilm.user.utils;
+
+import com.meetingfilm.utils.common.vo.BaseResponseVo;
+import org.junit.Test;
+
+/**
+ * 描述 :
+ * 作者 : 徐起超
+ * 时间 : 2020/12/6 8:39 下午
+ */
+public class UtilsTest {
+
+    @Test
+    public void name() {
+
+        BaseResponseVo baseResponseVo = new BaseResponseVo();
+        System.out.println(baseResponseVo.run(""));
+
+    }
+}
