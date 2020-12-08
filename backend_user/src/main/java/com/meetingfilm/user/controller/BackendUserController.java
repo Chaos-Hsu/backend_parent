118af744fa2b5e126ee15c679dce3834b83fac43
xqc <xqc>
2020-12-08 16:38:55 +0800

    task:

---
+++ backend_user/src/main/java/com/meetingfilm/user/controller/BackendUserController.java
@@ -0,0 +1,20 @@
+package com.meetingfilm.user.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户相关内容
+ *
+ * @author xqc
+ * @since 2020-12-07
+ */
+@RestController
+@RequestMapping("/user")
+public class BackendUserController {
+
+
+
+
+}
