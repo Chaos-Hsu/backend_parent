5bdb4940164f3c4b7a644f8517436ca5fe62642e
xqc <xqc>
2020-12-09 13:32:50 +0800

    task:演示工程

---
+++ backend_film/src/main/java/com/meetingfilm/film/BackendFilmApplication.java
@@ -0,0 +1,13 @@
+package com.meetingfilm.film;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFilmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendFilmApplication.class, args);
+    }
+
+}
