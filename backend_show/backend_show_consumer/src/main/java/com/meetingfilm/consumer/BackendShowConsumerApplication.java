c3b55abc7162a040dadd67fbdfb46184bad3cd74
xqc <xqc>
2020-12-09 09:07:07 +0800

    task:演示工程

--- backend_show/backend_show_consumer/src/main/java/com/meetingfilm/consumer/BackendShowConsumerApplication.java
+++ backend_show/backend_show_consumer/src/main/java/com/meetingfilm/consumer/BackendShowConsumerApplication.java
@@ -2,7 +2,13 @@ package com.meetingfilm.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+//可以适配大部分注册中心
+@EnableDiscoveryClient
+//只能用EureKa
+//@EnableEurekaClient
 @SpringBootApplication
 public class BackendShowConsumerApplication {
 
