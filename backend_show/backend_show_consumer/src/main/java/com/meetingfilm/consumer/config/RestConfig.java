861bd3c7c814e1176545f53814ed7c9994d10ccc
xqc <xqc>
2020-12-10 11:31:02 +0800

    task:ribbon

--- backend_show/backend_show_consumer/src/main/java/com/meetingfilm/consumer/config/RestConfig.java
+++ backend_show/backend_show_consumer/src/main/java/com/meetingfilm/consumer/config/RestConfig.java
@@ -1,9 +1,10 @@
 package com.meetingfilm.consumer.config;
 
-import com.meetingfilm.consumer.ribbon.rule.MyRule;
+import com.netflix.loadbalancer.IPing;
 import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.PingUrl;
 import com.netflix.loadbalancer.RoundRobinRule;
+import com.netflix.niws.loadbalancer.NIWSDiscoveryPing;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,9 +25,31 @@ public class RestConfig {
         return new RestTemplate();
     }
 
+    /**
+     * 负载均衡规则
+     *
+     * @return
+     */
     @Bean
     public IRule iRule() {
+        //自定义规则
         //return new MyRule();
+        // 随机循环
+        //return new RandomRule();
+        // 循序循环
         return new RoundRobinRule();
+    }
+
+    /**
+     * 校验服务是否存活机制
+     *
+     * @return
+     */
+    @Bean
+    public IPing iPing() {
+        //自己校验是否存活   是否安全连接(http,https)  如果/abc存活,服务存活
+        //return new PingUrl(false, "/abc");
+        //依赖euraka clic 的存活机制
+        return new NIWSDiscoveryPing();
     }
 }
